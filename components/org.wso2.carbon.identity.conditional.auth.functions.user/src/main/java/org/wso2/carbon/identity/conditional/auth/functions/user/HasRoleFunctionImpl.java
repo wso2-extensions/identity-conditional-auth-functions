57ef4a8469fffcbf2bc8d34953ba90af55780400
Pulasthi Mahawithana <pulasthi7@gmail.com>
2018-05-14 20:08:33 +0530

    Add HasRole Function with the Improvements.
    Resolves https://github.com/wso2/product-is/issues/3159
    Partial Fix for https://github.com/wso2/product-is/issues/3173

---
+++ components/org.wso2.carbon.identity.conditional.auth.functions.user/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/user/HasRoleFunctionImpl.java
@@ -0,0 +1,103 @@
+/*
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.wso2.carbon.identity.conditional.auth.functions.user;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.CarbonException;
+import org.wso2.carbon.core.util.AnonymousSessionUtil;
+import org.wso2.carbon.identity.application.authentication.framework.config.model.graph.js.JsAuthenticatedUser;
+import org.wso2.carbon.identity.application.authentication.framework.exception.FrameworkException;
+import org.wso2.carbon.identity.conditional.auth.functions.user.internal.UserFunctionsServiceHolder;
+import org.wso2.carbon.user.core.UserRealm;
+import org.wso2.carbon.user.core.UserStoreException;
+import org.wso2.carbon.user.core.UserStoreManager;
+
+import java.util.Arrays;
+
+/**
+ * Implementation of the {@link HasRoleFunction}
+ */
+public class HasRoleFunctionImpl implements HasRoleFunction {
+
+    private static final Log LOG = LogFactory.getLog(HasRoleFunctionImpl.class);
+
+    @Override
+    public boolean hasRole(JsAuthenticatedUser user, String roleName) {
+
+        boolean result = false;
+
+        String tenantDomain = user.getWrapped().getTenantDomain();
+        String userStoreDomain = user.getWrapped().getUserStoreDomain();
+        String username = user.getWrapped().getUserName();
+        try {
+            UserRealm userRealm = getUserRealm(user.getWrapped().getTenantDomain());
+            if (userRealm != null) {
+                UserStoreManager userStore = getUserStoreManager(tenantDomain, userRealm, userStoreDomain);
+                if (userStore != null) {
+                    result = Arrays.stream(userStore.getRoleListOfUser(username)).anyMatch(r -> r.equals(roleName));
+                }
+            }
+        } catch (FrameworkException e) {
+            LOG.error("Error in evaluating the function ", e);
+        } catch (UserStoreException e) {
+            LOG.error("Error in getting user from store at the function ", e);
+        }
+
+        return result;
+    }
+
+    private UserRealm getUserRealm(String tenantDomain) throws FrameworkException {
+
+        UserRealm realm;
+        try {
+            realm = AnonymousSessionUtil.getRealmByTenantDomain(UserFunctionsServiceHolder.getInstance()
+                    .getRegistryService(), UserFunctionsServiceHolder.getInstance().getRealmService(), tenantDomain);
+        } catch (CarbonException e) {
+            throw new FrameworkException(
+                    "Error occurred while retrieving the Realm for " + tenantDomain + " to retrieve user roles", e);
+        }
+        return realm;
+    }
+
+    private UserStoreManager getUserStoreManager(String tenantDomain, UserRealm realm, String userDomain)
+            throws FrameworkException {
+
+        UserStoreManager userStore = null;
+        try {
+            if (StringUtils.isNotBlank(userDomain)) {
+                userStore = realm.getUserStoreManager().getSecondaryUserStoreManager(userDomain);
+            } else {
+                userStore = realm.getUserStoreManager();
+            }
+
+            if (userStore == null) {
+                throw new FrameworkException(
+                        String.format("Invalid user store domain (given : %s) or tenant domain (given: %s).",
+                                userDomain, tenantDomain));
+            }
+        } catch (UserStoreException e) {
+            throw new FrameworkException(
+                    "Error occurred while retrieving the UserStoreManager from Realm for " + tenantDomain
+                            + " to retrieve user roles", e);
+        }
+        return userStore;
+    }
+}
---
+++ components/org.wso2.carbon.identity.conditional.auth.functions.user/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/user/internal/UserFunctionsServiceComponent.java
@@ -0,0 +1,127 @@
+/*
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.wso2.carbon.identity.conditional.auth.functions.user.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.carbon.identity.application.authentication.framework.JsFunctionRegistry;
+import org.wso2.carbon.identity.conditional.auth.functions.user.HasRoleFunction;
+import org.wso2.carbon.identity.conditional.auth.functions.user.HasRoleFunctionImpl;
+import org.wso2.carbon.registry.core.service.RegistryService;
+import org.wso2.carbon.user.core.service.RealmService;
+
+/**
+ * OSGi declarative services component which handled registration and unregistration of user conditional auth functions.
+ */
+
+@Component(
+        name = "identity.conditional.auth.functions.user.component",
+        immediate = true
+)
+public class UserFunctionsServiceComponent {
+
+    private static final Log LOG = LogFactory.getLog(UserFunctionsServiceComponent.class);
+
+    @Activate
+    protected void activate(ComponentContext ctxt) {
+
+        HasRoleFunction hasRoleFunctionImpl = new HasRoleFunctionImpl();
+        JsFunctionRegistry jsFunctionRegistry = UserFunctionsServiceHolder.getInstance().getJsFunctionRegistry();
+        jsFunctionRegistry.register(JsFunctionRegistry.Subsystem.SEQUENCE_HANDLER, "hasRole", hasRoleFunctionImpl);
+    }
+
+    @Deactivate
+    protected void deactivate(ComponentContext ctxt) {
+
+        JsFunctionRegistry jsFunctionRegistry = UserFunctionsServiceHolder.getInstance().getJsFunctionRegistry();
+        if (jsFunctionRegistry != null) {
+            jsFunctionRegistry.deRegister(JsFunctionRegistry.Subsystem.SEQUENCE_HANDLER, "hasRole");
+        }
+    }
+
+    @Reference(
+            name = "user.realmservice.default",
+            service = RealmService.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetRealmService"
+    )
+    protected void setRealmService(RealmService realmService) {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("RealmService is set in the conditional authentication user functions bundle");
+        }
+        UserFunctionsServiceHolder.getInstance().setRealmService(realmService);
+    }
+
+    protected void unsetRealmService(RealmService realmService) {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("RealmService is unset in the conditional authentication user functions bundle");
+        }
+        UserFunctionsServiceHolder.getInstance().setRealmService(null);
+    }
+
+    @Reference(
+            name = "registry.service",
+            service = RegistryService.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetRegistryService"
+    )
+    protected void setRegistryService(RegistryService registryService) {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("RegistryService is set in the conditional authentication user functions bundle");
+        }
+        UserFunctionsServiceHolder.getInstance().setRegistryService(registryService);
+    }
+
+    protected void unsetRegistryService(RegistryService registryService) {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("RegistryService is unset in the conditional authentication user functions bundle");
+        }
+        UserFunctionsServiceHolder.getInstance().setRegistryService(null);
+    }
+
+    @Reference(
+            service = JsFunctionRegistry.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetJsFunctionRegistry"
+    )
+    public void setJsFunctionRegistry(JsFunctionRegistry jsFunctionRegistry) {
+
+        UserFunctionsServiceHolder.getInstance().setJsFunctionRegistry(jsFunctionRegistry);
+    }
+
+    public void unsetJsFunctionRegistry(JsFunctionRegistry jsFunctionRegistry) {
+
+        UserFunctionsServiceHolder.getInstance().setJsFunctionRegistry(null);
+    }
+
+}
