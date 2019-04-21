81fe4d1b4d4c5429d1e86ee0b149f195963d1da2
senthalan <senthu18>
2019-04-21 20:18:16 +0530

    Add module for user store related adaptive functions

---
+++ components/org.wso2.carbon.identity.conditional.auth.functions.user.store/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/user/store/internal/UserStoreFunctionsServiceComponent.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.wso2.carbon.identity.conditional.auth.functions.user.store.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.carbon.identity.application.authentication.framework.JsFunctionRegistry;
+import org.wso2.carbon.identity.conditional.auth.functions.user.store.GetUserWithClaimValues;
+import org.wso2.carbon.identity.conditional.auth.functions.user.store.UserStoreFunctions;
+import org.wso2.carbon.registry.core.service.RegistryService;
+import org.wso2.carbon.user.core.service.RealmService;
+
+/**
+ * OSGi declarative services component which handles registration and de-registration of user conditional auth
+ * functions.
+ */
+@Component(
+        name = "identity.conditional.auth.functions.user.store.component",
+        immediate = true
+)
+public class UserStoreFunctionsServiceComponent {
+
+    private static final Log LOG = LogFactory.getLog(UserStoreFunctionsServiceComponent.class);
+
+    @Activate
+    protected void activate(ComponentContext ctxt) {
+
+        try {
+            UserStoreFunctions storeFunctions = new UserStoreFunctions();
+            JsFunctionRegistry jsFunctionRegistry = UserStoreFunctionsServiceHolder.getInstance()
+                    .getJsFunctionRegistry();
+
+            jsFunctionRegistry.register(JsFunctionRegistry.Subsystem.SEQUENCE_HANDLER, "getUniqueUserWithClaimValues",
+                    (GetUserWithClaimValues) storeFunctions::getUniqueUserWithClaimValues);
+        } catch (Throwable e) {
+            LOG.error("Error occurred during conditional authentication user functions bundle activation. ", e);
+
+        }
+    }
+
+    @Deactivate
+    protected void deactivate(ComponentContext ctxt) {
+
+        JsFunctionRegistry jsFunctionRegistry = UserStoreFunctionsServiceHolder.getInstance()
+                .getJsFunctionRegistry();
+        if (jsFunctionRegistry != null) {
+            jsFunctionRegistry.deRegister(JsFunctionRegistry.Subsystem.SEQUENCE_HANDLER,
+                    "getUserListWithClaimValue");
+            jsFunctionRegistry.deRegister(JsFunctionRegistry.Subsystem.SEQUENCE_HANDLER, "getUserClaimValue");
+        }
+    }
+
+    @Reference(
+            name = "user.realmservice.default",
+            service = RealmService.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetRealmService"
+    )
+    protected void setRealmService(RealmService realmService) {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("RealmService is set in the conditional authentication user functions bundle");
+        }
+        UserStoreFunctionsServiceHolder.getInstance().setRealmService(realmService);
+    }
+
+    protected void unsetRealmService(RealmService realmService) {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("RealmService is unset in the conditional authentication user functions bundle");
+        }
+        UserStoreFunctionsServiceHolder.getInstance().setRealmService(null);
+    }
+
+    @Reference(
+            name = "registry.service",
+            service = RegistryService.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetRegistryService"
+    )
+    protected void setRegistryService(RegistryService registryService) {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("RegistryService is set in the conditional authentication user functions bundle");
+        }
+        UserStoreFunctionsServiceHolder.getInstance().setRegistryService(registryService);
+    }
+
+    protected void unsetRegistryService(RegistryService registryService) {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("RegistryService is unset in the conditional authentication user functions bundle");
+        }
+        UserStoreFunctionsServiceHolder.getInstance().setRegistryService(null);
+    }
+
+    @Reference(
+            service = JsFunctionRegistry.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetJsFunctionRegistry"
+    )
+    public void setJsFunctionRegistry(JsFunctionRegistry jsFunctionRegistry) {
+
+        UserStoreFunctionsServiceHolder.getInstance().setJsFunctionRegistry(jsFunctionRegistry);
+    }
+
+    public void unsetJsFunctionRegistry(JsFunctionRegistry jsFunctionRegistry) {
+
+        UserStoreFunctionsServiceHolder.getInstance().setJsFunctionRegistry(null);
+    }
+
+}
