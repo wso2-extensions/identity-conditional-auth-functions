f24a73d205841b9c1ad0ac9de442b8613ba3ac10
Gayashan Bombuwala <gayashanbc@gmail.com>
2019-08-27 12:25:00 +0530

    Remove usage of JavaScript callbacks in EnsureMaxSessionCountNotExceededForUserFunction.java

--- components/org.wso2.carbon.identity.conditional.auth.functions.user/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/user/EnsureMaxSessionCountNotExceededForUserFunctionImpl.java
+++ components/org.wso2.carbon.identity.conditional.auth.functions.user/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/user/EnsureMaxSessionCountNotExceededForUserFunctionImpl.java
@@ -21,7 +21,6 @@ package org.wso2.carbon.identity.conditional.auth.functions.user;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.config.model.graph.JsGraphBuilder;
-import org.wso2.carbon.identity.application.authentication.framework.config.model.graph.SerializableJsFunction;
 import org.wso2.carbon.identity.application.authentication.framework.config.model.graph.js.JsAuthenticatedUser;
 import org.wso2.carbon.identity.application.authentication.framework.exception.FrameworkException;
 import org.wso2.carbon.identity.application.authentication.framework.exception.UserSessionException;
@@ -32,46 +31,39 @@ import org.wso2.carbon.identity.conditional.auth.functions.user.model.UserAgent;
 import org.wso2.carbon.user.core.UserRealm;
 import org.wso2.carbon.user.core.UserStoreManager;
 
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class EnsureMaxSessionCountNotExceededForUserFunctionImpl implements EnsureMaxSessionCountNotExceededForUserFunction {
+/**
+ * Function to ensure that the active session count of the specified user does not exceed the given maximum limit.
+ * This will prompt to terminate the unwanted sessions or to deny the login,
+ * if the specified maximum session count is exceeded.
+ */
+public class EnsureMaxSessionCountNotExceededForUserFunctionImpl
+        implements EnsureMaxSessionCountNotExceededForUserFunction {
 
     private static final Log LOG = LogFactory.getLog(EnsureMaxSessionCountNotExceededForUserFunctionImpl.class);
 
     @Override
     public void ensureMaxSessionCountNotExceededForUser(JsAuthenticatedUser user, int maxSessionCount) {
+
         List<String[]> sessionsForUser = null;
-        Map<String, Object> data = null;
-        Map<String, Object> handlers = null;
+        Map<String, String> locator;
+        Map<String, Object> data;
         String tenantDomain = user.getWrapped().getTenantDomain();
         String userStoreDomain = user.getWrapped().getUserStoreDomain();
         String username = user.getWrapped().getUserName();
 
-        final String KILL_SESSIONS_FUNCTION[] =
-                {"function (context) {",
-                        "   var sessions = context.request.params.sessionsToKill;",
-                        "   for (var key in sessions) {",
-                        "       KillUserSession(context.currentKnownSubject, sessions[key]);",
-                        "    }",
-                        "}"};
-        final String ABORT_LOGIN_FUNCTION[] =
-                {"function(context) {",
-                        "   sendError( '', {",
-                        "                       'status': 'Login Aborted',",
-                        "                       'statusMsg': 'Maximum limit of allowed concurrent active sessions exceeded.'",
-                        "                   }",
-                        "   );",
-                        "}"};
-
         try {
-            UserRealm userRealm = Utils.getUserRealm(user.getWrapped().getTenantDomain());
+            UserRealm userRealm = Utils.getUserRealm(tenantDomain);
             if (userRealm != null) {
                 UserStoreManager userStore = Utils.getUserStoreManager(tenantDomain, userRealm, userStoreDomain);
                 if (userStore != null) {
-                    String userId = UserSessionStore.getInstance().getUserId(username, Utils.getTenantId(tenantDomain), userStoreDomain);
+                    String userId = UserSessionStore.getInstance()
+                            .getUserId(username, Utils.getTenantId(tenantDomain), userStoreDomain);
                     sessionsForUser = UserFunctionsServiceHolder.getInstance()
                             .getUserSessionManagementService().getSessionsByUserId(userId).stream()
                             .map(userSession -> {
@@ -96,15 +88,17 @@ public class EnsureMaxSessionCountNotExceededForUserFunctionImpl implements Ensu
         }
 
         if (sessionsForUser.size() >= maxSessionCount) {
+
+            locator = new HashMap<>();
             data = new HashMap<>();
-            handlers = new HashMap<>();
+
+            locator.put("redirectUrl", "/authenticationendpoint/handle-multiple-sessions.do");
 
             data.put("maxSessionCount", maxSessionCount);
             data.put("sessions", sessionsForUser.toArray());
-            handlers.put("onKillActiveSessions", new SerializableJsFunction(String.join(" ", KILL_SESSIONS_FUNCTION), true));
-            handlers.put("onAbortLogin", new SerializableJsFunction(String.join(" ", ABORT_LOGIN_FUNCTION), true));
+            data.put("encodedUsername", new String(Base64.getEncoder().encode(username.getBytes())));
 
-            JsGraphBuilder.getCurrentBuilder().addShowPrompt("multipleActiveSessions", data, handlers);
+            JsGraphBuilder.getCurrentBuilder().addShowPromptExternal(locator, data, null);
         }
     }
 }
