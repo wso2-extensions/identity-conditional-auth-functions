8436489dcbe65d23646e625d4a33ea7a6eee2c23
AnuradhaSK <anuradha199528@gmail.com>
2021-04-10 21:07:21 +0530

    use idp user id claim uri as external subject when claim mappings are defined

--- components/org.wso2.carbon.identity.conditional.auth.functions.user/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/user/SetAccountAssociationToLocalUserImpl.java
+++ components/org.wso2.carbon.identity.conditional.auth.functions.user/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/user/SetAccountAssociationToLocalUserImpl.java
@@ -18,12 +18,23 @@
 
 package org.wso2.carbon.identity.conditional.auth.functions.user;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.config.model.graph.js.JsAuthenticatedUser;
+import org.wso2.carbon.identity.application.common.model.ClaimConfig;
+import org.wso2.carbon.identity.application.common.model.ClaimMapping;
+import org.wso2.carbon.identity.application.common.model.IdentityProvider;
 import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.identity.user.profile.mgt.UserProfileException;
 import org.wso2.carbon.identity.user.profile.mgt.dao.UserProfileMgtDAO;
+import org.wso2.carbon.idp.mgt.IdentityProviderManagementException;
+import org.wso2.carbon.idp.mgt.IdentityProviderManagementService;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 
 public class SetAccountAssociationToLocalUserImpl implements SetAccountAssociationToLocalUser {
 
@@ -33,29 +44,75 @@ public class SetAccountAssociationToLocalUserImpl implements SetAccountAssociati
     public void doAssociationWithLocalUser(JsAuthenticatedUser federatedUser, String username, String tenantDomain,
                                            String userStoreDomainName) {
 
-        if (federatedUser != null) {
-            if (federatedUser.getWrapped().isFederatedUser()) {
-                String externalSubject = federatedUser.getWrapped().getAuthenticatedSubjectIdentifier();
-                String externalIdpName = federatedUser.getWrapped().getFederatedIdPName();
-                if (externalSubject != null && externalIdpName != null) {
-                    associateID(externalIdpName, externalSubject, username, tenantDomain, userStoreDomainName);
+        String federatedIdpName = null;
+        try {
+            if (federatedUser != null) {
+                if (federatedUser.getWrapped().isFederatedUser()) {
+                    federatedIdpName = federatedUser.getWrapped().getFederatedIdPName();
+                    String userIdClaimURI = getUserIdClaimURI(federatedIdpName);
+                    String externalSubject;
+                    if (StringUtils.isNotEmpty(userIdClaimURI)) {
+                        externalSubject = federatedUser.getWrapped().getUserAttributes().entrySet().stream().filter(
+                                userAttribute -> userAttribute.getKey().getRemoteClaim().getClaimUri()
+                                        .equals(userIdClaimURI))
+                                .map(Map.Entry::getValue)
+                                .findFirst()
+                                .orElse(null);
+                    } else {
+                        externalSubject = federatedUser.getWrapped().getAuthenticatedSubjectIdentifier();
+                    }
+                    String externalIdpName = federatedUser.getWrapped().getFederatedIdPName();
+                    if (externalSubject != null && externalIdpName != null) {
+                        associateID(externalIdpName, externalSubject, username, tenantDomain, userStoreDomainName);
+                    } else {
+                        if (log.isDebugEnabled()) {
+                            log.debug(" Authenticated user or External IDP may be null " + " Authenticated User: " +
+                                    externalSubject + " and the External IDP name: " + externalIdpName);
+                        }
+                    }
                 } else {
                     if (log.isDebugEnabled()) {
-                        log.debug(" Authenticated user or External IDP may be null " + " Authenticated User: " +
-                                externalSubject + " and the External IDP name: " + externalIdpName);
+                        log.debug("User " + federatedUser.getWrapped().getAuthenticatedSubjectIdentifier() + " " +
+                                "is not a federated user.");
                     }
                 }
             } else {
                 if (log.isDebugEnabled()) {
-                    log.debug("User " + federatedUser.getWrapped().getAuthenticatedSubjectIdentifier() + " " +
-                            "is not a federated user." );
+                    log.debug(" Federated user is null ");
                 }
             }
-        } else {
-            if (log.isDebugEnabled()) {
-                log.debug(" Federated user is null ");
-            }
+        } catch (IdentityProviderManagementException e) {
+            String msg =
+                    "Error while retrieving identity provider by name: " + federatedIdpName;
+            log.error(msg, e);
         }
+    }
+
+    private String getUserIdClaimURI(String federatedIdpName) throws IdentityProviderManagementException {
+
+        String userIdClaimURI = null;
+        String userNameLocalClaim = "http://wso2.org/claims/username";
+        IdentityProviderManagementService idpManager = new IdentityProviderManagementService();
+        IdentityProvider idp = idpManager.getIdPByName(federatedIdpName);
+        if (idp == null) {
+            return null;
+        }
+        ClaimConfig claimConfigs = idp.getClaimConfig();
+        if (claimConfigs == null) {
+            return null;
+        }
+        ClaimMapping[] claimMappings = claimConfigs.getClaimMappings();
+        if (CollectionUtils.isEmpty(Collections.singleton(userIdClaimURI))) {
+            return null;
+        }
+        ClaimMapping userNameClaimMapping = Arrays.stream(claimMappings).filter(claimMapping ->
+                StringUtils.equals(userNameLocalClaim, claimMapping.getLocalClaim().getClaimUri()))
+                .findFirst()
+                .orElse(null);
+        if (userNameClaimMapping != null) {
+            userIdClaimURI = userNameClaimMapping.getRemoteClaim().getClaimUri();
+        }
+        return userIdClaimURI;
     }
 
     /**
