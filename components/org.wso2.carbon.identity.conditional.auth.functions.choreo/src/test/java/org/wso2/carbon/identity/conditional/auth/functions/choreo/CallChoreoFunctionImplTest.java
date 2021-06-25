edf54645ed59091d5ee6d00b6000ce7b22bb6868
ChanikaRuchini <chanikaruchini.16@cse.mrt.ac.lk>
2021-06-25 12:47:19 +0530

    update unit tests

--- components/org.wso2.carbon.identity.conditional.auth.functions.choreo/src/test/java/org/wso2/carbon/identity/conditional/auth/functions/choreo/CallChoreoFunctionImplTest.java
+++ components/org.wso2.carbon.identity.conditional.auth.functions.choreo/src/test/java/org/wso2/carbon/identity/conditional/auth/functions/choreo/CallChoreoFunctionImplTest.java
@@ -25,6 +25,7 @@ import org.wso2.carbon.identity.application.authentication.framework.config.mode
 import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
 import org.wso2.carbon.identity.application.authentication.framework.dao.impl.CacheBackedLongWaitStatusDAO;
 import org.wso2.carbon.identity.application.authentication.framework.dao.impl.LongWaitStatusDAOImpl;
+import org.wso2.carbon.identity.application.authentication.framework.exception.FrameworkException;
 import org.wso2.carbon.identity.application.authentication.framework.internal.FrameworkServiceDataHolder;
 import org.wso2.carbon.identity.application.authentication.framework.store.LongWaitStatusStoreService;
 import org.wso2.carbon.identity.application.common.model.LocalAndOutboundAuthenticationConfig;
@@ -35,6 +36,7 @@ import org.wso2.carbon.identity.common.testng.WithCarbonHome;
 import org.wso2.carbon.identity.common.testng.WithH2Database;
 import org.wso2.carbon.identity.common.testng.WithMicroService;
 import org.wso2.carbon.identity.common.testng.WithRealmService;
+import org.wso2.carbon.identity.conditional.auth.functions.choreo.internal.ChoreoFunctionServiceHolder;
 import org.wso2.carbon.identity.conditional.auth.functions.common.internal.FunctionsDataHolder;
 import org.wso2.carbon.identity.conditional.auth.functions.test.utils.sequence.JsSequenceHandlerAbstractTest;
 import org.wso2.carbon.identity.conditional.auth.functions.test.utils.sequence.JsTestException;
@@ -79,7 +81,7 @@ public class CallChoreoFunctionImplTest extends JsSequenceHandlerAbstractTest {
     }
 
     @Test
-    public void testRiskScore() throws JsTestException, NoSuchFieldException, IllegalAccessException {
+    public void testRiskScore() throws JsTestException, NoSuchFieldException, IllegalAccessException, FrameworkException {
 
         FunctionsDataHolder functionsDataHolder = Mockito.mock(FunctionsDataHolder.class);
         Field functionsDataHolderInstance = FunctionsDataHolder.class.getDeclaredField("instance");
@@ -90,6 +92,9 @@ public class CallChoreoFunctionImplTest extends JsSequenceHandlerAbstractTest {
         frameworkServiceDataHolderInstance.setAccessible(true);
         FrameworkServiceDataHolder availableInstance = (FrameworkServiceDataHolder) frameworkServiceDataHolderInstance
                 .get(null);
+        ClientManager clientManager = new ClientManager();
+        ChoreoFunctionServiceHolder.getInstance().setClientManager(clientManager);
+
 
         LongWaitStatusDAOImpl daoImpl = new LongWaitStatusDAOImpl();
         CacheBackedLongWaitStatusDAO cacheBackedDao = new CacheBackedLongWaitStatusDAO(daoImpl);
