7ab4331e8bac16d5aba2290dc17e8486b46d4807
Janak Amarasena <janakamarasena@gmail.com>
2021-09-09 10:16:07 +0530

    Refactor http functions

---
+++ components/org.wso2.carbon.identity.conditional.auth.functions.http/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/http/AbstractHTTPFunction.java
@@ -0,0 +1,102 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com).
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.wso2.carbon.identity.conditional.auth.functions.http;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ConnectTimeoutException;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.wso2.carbon.identity.application.authentication.framework.AsyncProcess;
+import org.wso2.carbon.identity.application.authentication.framework.config.model.graph.JsGraphBuilder;
+import org.wso2.carbon.identity.conditional.auth.functions.common.utils.ConfigProvider;
+import org.wso2.carbon.identity.conditional.auth.functions.common.utils.Constants;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Abstract class for handling http calls.
+ */
+public abstract class AbstractHTTPFunction {
+
+    private static final Log LOG = LogFactory.getLog(AbstractHTTPFunction.class);
+    protected static final String TYPE_APPLICATION_JSON = "application/json";
+
+    private CloseableHttpClient client;
+
+    public AbstractHTTPFunction() {
+
+        RequestConfig config = RequestConfig.custom()
+                .setConnectTimeout(ConfigProvider.getInstance().getConnectionTimeout())
+                .setConnectionRequestTimeout(ConfigProvider.getInstance().getConnectionRequestTimeout())
+                .setSocketTimeout(ConfigProvider.getInstance().getReadTimeout())
+                .build();
+        client = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+    }
+
+    protected void executeHttpMethod(HttpUriRequest request, Map<String, Object> eventHandlers) {
+
+        AsyncProcess asyncProcess = new AsyncProcess((context, asyncReturn) -> {
+            JSONObject json = null;
+            int responseCode;
+            String outcome;
+            String epUrl = request.getURI().toString();
+
+            try (CloseableHttpResponse response = client.execute(request)){
+                responseCode = response.getStatusLine().getStatusCode();
+                if (responseCode == 200) {
+                    outcome = Constants.OUTCOME_SUCCESS;
+                    String jsonString = EntityUtils.toString(response.getEntity());
+                    JSONParser parser = new JSONParser();
+                    json = (JSONObject) parser.parse(jsonString);
+                } else {
+                    outcome = Constants.OUTCOME_FAIL;
+                }
+
+            } catch (IllegalArgumentException e) {
+                LOG.error("Invalid Url: " + epUrl, e);
+                outcome = Constants.OUTCOME_FAIL;
+            } catch (ConnectTimeoutException e) {
+                LOG.error("Error while waiting to connect to " + epUrl, e);
+                outcome = Constants.OUTCOME_TIMEOUT;
+            } catch (SocketTimeoutException e) {
+                LOG.error("Error while waiting for data from " + epUrl, e);
+                outcome = Constants.OUTCOME_TIMEOUT;
+            } catch (IOException e) {
+                LOG.error("Error while calling endpoint. ", e);
+                outcome = Constants.OUTCOME_FAIL;
+            } catch (ParseException e) {
+                LOG.error("Error while parsing response. ", e);
+                outcome = Constants.OUTCOME_FAIL;
+            }
+
+            asyncReturn.accept(context, json != null ? json : Collections.emptyMap(), outcome);
+        });
+        JsGraphBuilder.addLongWaitProcess(asyncProcess, eventHandlers);
+    }
+}
--- components/org.wso2.carbon.identity.conditional.auth.functions.http/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/http/HTTPPostFunctionImpl.java
+++ components/org.wso2.carbon.identity.conditional.auth.functions.http/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/http/HTTPPostFunctionImpl.java
@@ -20,25 +20,11 @@ package org.wso2.carbon.identity.conditional.auth.functions.http;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.wso2.carbon.identity.application.authentication.framework.AsyncProcess;
-import org.wso2.carbon.identity.application.authentication.framework.config.model.graph.JsGraphBuilder;
-import org.wso2.carbon.identity.conditional.auth.functions.common.utils.ConfigProvider;
-import org.wso2.carbon.identity.conditional.auth.functions.common.utils.Constants;
 
-import java.io.IOException;
-import java.net.SocketTimeoutException;
-import java.util.Collections;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 import static org.apache.http.HttpHeaders.ACCEPT;
@@ -48,73 +34,27 @@ import static org.apache.http.HttpHeaders.CONTENT_TYPE;
 /**
  * Implementation of the {@link HTTPPostFunction}
  */
-public class HTTPPostFunctionImpl implements HTTPPostFunction {
+public class HTTPPostFunctionImpl extends AbstractHTTPFunction implements HTTPPostFunction {
 
     private static final Log LOG = LogFactory.getLog(HTTPPostFunctionImpl.class);
-    private static final String TYPE_APPLICATION_JSON = "application/json";
-
-    private CloseableHttpClient client;
 
     public HTTPPostFunctionImpl() {
 
-        RequestConfig config = RequestConfig.custom()
-                .setConnectTimeout(ConfigProvider.getInstance().getConnectionTimeout())
-                .setConnectionRequestTimeout(ConfigProvider.getInstance().getConnectionRequestTimeout())
-                .setSocketTimeout(ConfigProvider.getInstance().getReadTimeout())
-                .build();
-        client = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+       super();
     }
 
     @Override
     public void httpPost(String epUrl, Map<String, Object> payloadData, Map<String, Object> eventHandlers) {
 
-        AsyncProcess asyncProcess = new AsyncProcess((context, asyncReturn) -> {
-            JSONObject json = null;
-            int responseCode;
-            String outcome;
+            HttpPost request = new HttpPost(epUrl);
+            request.setHeader(ACCEPT, TYPE_APPLICATION_JSON);
+            request.setHeader(CONTENT_TYPE, TYPE_APPLICATION_JSON);
 
-            try {
-                HttpPost request = new HttpPost(epUrl);
-                request.setHeader(ACCEPT, TYPE_APPLICATION_JSON);
-                request.setHeader(CONTENT_TYPE, TYPE_APPLICATION_JSON);
-
-                JSONObject jsonObject = new JSONObject();
-                for (Map.Entry<String, Object> dataElements : payloadData.entrySet()) {
-                    jsonObject.put(dataElements.getKey(), dataElements.getValue());
-                }
-                request.setEntity(new StringEntity(jsonObject.toJSONString()));
-
-                try (CloseableHttpResponse response = client.execute(request)) {
-                    responseCode = response.getStatusLine().getStatusCode();
-
-                    if (responseCode == 200) {
-                        outcome = Constants.OUTCOME_SUCCESS;
-                        String jsonString = EntityUtils.toString(response.getEntity());
-                        JSONParser parser = new JSONParser();
-                        json = (JSONObject) parser.parse(jsonString);
-                    } else {
-                        outcome = Constants.OUTCOME_FAIL;
-                    }
-                }
-            } catch (IllegalArgumentException e) {
-                LOG.error("Invalid Url: " + epUrl, e);
-                outcome = Constants.OUTCOME_FAIL;
-            } catch (ConnectTimeoutException e) {
-                LOG.error("Error while waiting to connect to " + epUrl, e);
-                outcome = Constants.OUTCOME_TIMEOUT;
-            } catch (SocketTimeoutException e) {
-                LOG.error("Error while waiting for data from " + epUrl, e);
-                outcome = Constants.OUTCOME_TIMEOUT;
-            } catch (IOException e) {
-                LOG.error("Error while calling endpoint. ", e);
-                outcome = Constants.OUTCOME_FAIL;
-            } catch (ParseException e) {
-                LOG.error("Error while parsing response. ", e);
-                outcome = Constants.OUTCOME_FAIL;
+            JSONObject jsonObject = new JSONObject();
+            for (Map.Entry<String, Object> dataElements : payloadData.entrySet()) {
+                jsonObject.put(dataElements.getKey(), dataElements.getValue());
             }
-
-            asyncReturn.accept(context, json != null ? json : Collections.emptyMap(), outcome);
-        });
-        JsGraphBuilder.addLongWaitProcess(asyncProcess, eventHandlers);
+            request.setEntity(new StringEntity(jsonObject.toJSONString(), StandardCharsets.UTF_8));
+            executeHttpMethod(request, eventHandlers);
     }
 }
