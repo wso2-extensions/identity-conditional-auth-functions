46887e547b953177b198e9c324432bf715f3134b
Pasindu Yeshan Abeysinghe <pasindua.18@cse.mrt.ac.lk>
2022-08-08 19:25:14 +0530

    Add http header feature to HTTP POST function

--- components/org.wso2.carbon.identity.conditional.auth.functions.http/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/http/HTTPPostFunctionImpl.java
+++ components/org.wso2.carbon.identity.conditional.auth.functions.http/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/http/HTTPPostFunctionImpl.java
@@ -20,11 +20,17 @@ package org.wso2.carbon.identity.conditional.auth.functions.http;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.simple.JSONObject;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.apache.http.HttpHeaders.ACCEPT;
@@ -44,17 +50,61 @@ public class HTTPPostFunctionImpl extends AbstractHTTPFunction implements HTTPPo
     }
 
     @Override
-    public void httpPost(String epUrl, Map<String, Object> payloadData, Map<String, Object> eventHandlers) {
+    public void httpPost(String epUrl, Object... params) {
 
-            HttpPost request = new HttpPost(epUrl);
-            request.setHeader(ACCEPT, TYPE_APPLICATION_JSON);
+        Map<String, Object> eventHandlers = new HashMap<>();
+        Map<String, Object> payloadData = new HashMap<>();
+        Map<String, String> headers = new HashMap<>();
+
+        if (params.length == 2) {
+            if (params[0] instanceof  Map && params[1] instanceof Map) {
+                payloadData = (Map<String, Object>) params[0];
+                eventHandlers = (Map<String, Object>) params[1];
+            }
+        } else if (params.length == 3) {
+            if (params[0] instanceof  Map) {
+                payloadData = (Map<String, Object>) params[0];
+            }
+            if (params[1] instanceof  Map) {
+                headers = (Map<String, String>) params[1];
+            }
+            if (params[2] instanceof Map) {
+                eventHandlers = (Map<String, Object>) params[2];
+            }
+        }
+
+        HttpPost request = new HttpPost(epUrl);
+        if (headers != null) {
+            // Check if the "Content-Type" is in headers map else set APPLICATION_JSON as default "Content-Type".
+            if (!headers.containsKey(CONTENT_TYPE)){
+                request.setHeader(CONTENT_TYPE, TYPE_APPLICATION_JSON);
+            }
+            for (Map.Entry<String, String> dataElements : headers.entrySet()) {
+                request.setHeader(dataElements.getKey(), dataElements.getValue());
+            }
+        } else {
+            // If no headers were given, set Content-Type to "application/json".
             request.setHeader(CONTENT_TYPE, TYPE_APPLICATION_JSON);
+        }
+        request.setHeader(ACCEPT, TYPE_APPLICATION_JSON);
 
+        /*
+          For the header "Content-Type : application/x-www-form-urlencoded"
+          request body data is set to UrlEncodedFormEntity format.
+         */
+        if (headers != null && TYPE_APPLICATION_FORM_URLENCODED.equals(headers.get(CONTENT_TYPE))) {
+            List<NameValuePair> headersList = new ArrayList<NameValuePair>();
+            for (Map.Entry<String, Object> dataElements : payloadData.entrySet()) {
+                headersList.add(new BasicNameValuePair(dataElements.getKey(), (String) dataElements.getValue()));
+            }
+            request.setEntity(new UrlEncodedFormEntity(headersList, StandardCharsets.UTF_8));
+        } else {
             JSONObject jsonObject = new JSONObject();
             for (Map.Entry<String, Object> dataElements : payloadData.entrySet()) {
                 jsonObject.put(dataElements.getKey(), dataElements.getValue());
             }
             request.setEntity(new StringEntity(jsonObject.toJSONString(), StandardCharsets.UTF_8));
-            executeHttpMethod(request, eventHandlers);
+        }
+        executeHttpMethod(request, eventHandlers);
     }
 }
