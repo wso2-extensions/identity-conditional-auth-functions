79942d6847ec1dc9bf3a51d872ca8cc47d86aa1d
sandushi <sandushidileka2@gmail.com>
2022-11-23 17:30:35 +0530

    Address the comments

--- components/org.wso2.carbon.identity.conditional.auth.functions.http/src/test/java/org/wso2/carbon/identity/conditional/auth/functions/http/CryptoProviderTest.java
+++ components/org.wso2.carbon.identity.conditional.auth.functions.http/src/test/java/org/wso2/carbon/identity/conditional/auth/functions/http/CryptoProviderTest.java
@@ -82,8 +82,8 @@ public class CryptoProviderTest implements InternalCryptoProvider {
     /*
      * Method to generate a secret key for AES algorithm with a given secret key.
      */
-    private static Key generateKey(String secretKey) throws Exception
-    {
+    private static Key generateKey(String secretKey) throws Exception {
+        
         Key key = new SecretKeySpec(secretKey.getBytes(), ALGO);
         return key;
     }
