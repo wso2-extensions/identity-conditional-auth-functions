28507aa3803fdb12a38731c05178e9771ae853b8
Afra Hussaindeen <mhfafra@gmail.com>
2023-11-22 09:38:23 +0530

    Add support function to filter authenticators

---
+++ components/org.wso2.carbon.identity.conditional.auth.functions.utils/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/utils/internal/UtilsFunctionServiceComponent.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.conditional.auth.functions.utils.internal;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.carbon.identity.application.authentication.framework.JsFunctionRegistry;
+import org.wso2.carbon.identity.conditional.auth.functions.utils.FilterAuthenticatorsFunction;
+import org.wso2.carbon.identity.conditional.auth.functions.utils.FilterAuthenticatorsFunctionImpl;
+
+/**
+ * OSGi declarative services component which handles registration and de-registration of utils related
+ * conditional auth functions.
+ */
+@Component(
+        name = "identity.conditional.auth.functions.utils.component",
+        immediate = true
+)
+public class UtilsFunctionServiceComponent {
+
+    @Activate
+    protected void activate(ComponentContext ctxt) {
+
+        FilterAuthenticatorsFunction filterAuthenticatorsFunctionImpl = new FilterAuthenticatorsFunctionImpl();
+        JsFunctionRegistry jsFunctionRegistry = UtilsFunctionServiceHolder.getInstance().getJsFunctionRegistry();
+        jsFunctionRegistry.register(JsFunctionRegistry.Subsystem.SEQUENCE_HANDLER, "filterAuthenticators",
+                filterAuthenticatorsFunctionImpl);
+    }
+
+    @Deactivate
+    protected void deactivate(ComponentContext ctxt) {
+
+        JsFunctionRegistry jsFunctionRegistry = UtilsFunctionServiceHolder.getInstance().getJsFunctionRegistry();
+        if (jsFunctionRegistry != null) {
+            jsFunctionRegistry.deRegister(JsFunctionRegistry.Subsystem.SEQUENCE_HANDLER, "filterAuthenticators");
+        }
+    }
+
+    @Reference(
+            service = JsFunctionRegistry.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetJsFunctionRegistry"
+    )
+    public void setJsFunctionRegistry(JsFunctionRegistry jsFunctionRegistry) {
+
+        UtilsFunctionServiceHolder.getInstance().setJsFunctionRegistry(jsFunctionRegistry);
+    }
+
+    public void unsetJsFunctionRegistry(JsFunctionRegistry jsFunctionRegistry) {
+
+        UtilsFunctionServiceHolder.getInstance().setJsFunctionRegistry(null);
+    }
+}
