6ba0c9e902ffeee4057a62cea73e4b16161c3aee
Sachin-Mamoru <sachinm@wso2.com>
2024-05-21 14:07:11 +0530

    resolved build issue

--- components/org.wso2.carbon.identity.conditional.auth.functions.common/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/common/utils/Constants.java
+++ components/org.wso2.carbon.identity.conditional.auth.functions.common/src/main/java/org/wso2/carbon/identity/conditional/auth/functions/common/utils/Constants.java
@@ -67,6 +67,8 @@ public class Constants {
             public static final String REQUEST_TOKEN_HTTP_POST = "request-token-http-post";
             public static final String INVOKE_API_HTTP_GET = "invoke-api-http-get";
             public static final String INVOKE_API_HTTP_POST = "invoke-api-http-post";
+            public static final String VALIDATE_INPUT_PARAMS = "validate-input-parameters";
+            public static final String UPDATE_USER_PASSWORD = "update-user-password";
         }
 
         /**
